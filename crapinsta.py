e12614b19b42703bb52a409a1dccedd232f7fcc7
Alfonso Kim <alfonso.kimq@gmail.com>
2018-11-06 21:52:13 -0600

    fix in requirements

--- crapinsta.py
+++ crapinsta.py
@@ -3,20 +3,43 @@ from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
 import requests
 import os
+import time
+from selenium.common.exceptions import NoSuchElementException
 
 ## claudiacarnevali
+
+def scroll_page(driver):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, 0);")
+    time.sleep(1)
+
 
 def must_not_fap(args):
 
     driver = webdriver.Firefox()
+
+    for anchor in driver.find_elements_by_xpath('//a'):
+        print anchor
+    return
+
     print 'Downloading photos from %s account' % args.profile
     driver.get('https://www.instagram.com/%s/' % args.profile)
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     if not os.path.exists(args.profile):
         print 'creating folder %s' % args.profile
         os.makedirs(args.profile)
+
+    driver.implicitly_wait(1)
+
+    # Click en boton de cargar mas
+    more = driver.find_element_by_partial_link_text("anchor_text")
+
+    for i in range(1000 // 12):
+        print 'sss'
+        scroll_page(driver)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     img_count = 0
     for x in soup.findAll('img'):
@@ -29,6 +52,10 @@ def must_not_fap(args):
         if args.limit > 0 and img_count >= args.limit:
             break
         img_count += 1
+
+    driver.close()
+
+
 
 if __name__ == '__main__':
     import argparse
